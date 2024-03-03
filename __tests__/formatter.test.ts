d49b6eff00e670e258711d4872400d924556a61c
Jizu <jizu.sun@philips.com>
2024-03-03 16:08:59 +0800

    tdd: all green

--- __tests__/formatter.test.ts
+++ __tests__/formatter.test.ts
@@ -6,11 +6,12 @@ import { formatDockerTags } from '../src/formatter'
 import { expect } from '@jest/globals'
 
 describe('formatter.ts', () => {
-  it('converts docker tags string into a markdown bullet list string', async () => {
-    const input = 'ghcr.io/jizusun/action-detect-secrets:pr-3 ghcr.io/jizusun/action-detect-secrets:sha-7200ec4'
+  it('converts a string of docker tags into a string of markdown bullet list, each tag as inline code', async () => {
+    const input =
+      'ghcr.io/jizusun/action-detect-secrets:pr-3\nghcr.io/jizusun/action-detect-secrets:sha-7200ec4'
     const actual = formatDockerTags(input)
-    const expected = `- ghcr.io/jizusun/action-detect-secrets:pr-3
-- ghcr.io/jizusun/action-detect-secrets:sha-7200ec4`
+    const expected = `- \`ghcr.io/jizusun/action-detect-secrets:pr-3\`
+- \`ghcr.io/jizusun/action-detect-secrets:sha-7200ec4\``
     expect(actual).toBe(expected)
   })
 })
