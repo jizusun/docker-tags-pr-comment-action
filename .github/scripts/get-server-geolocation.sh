4a94b5c41e3b169ab729155268e26a2a6eb42be0
Jizu <jizu.sun@philips.com>
2024-05-17 20:44:59 +0800

    curl aliyun shanghai

---
+++ .github/scripts/get-server-geolocation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "No URL provided. Usage: ./script.sh <url>"
+    exit 1
+fi
+url=$1
+echo "The URL is $url"
+domain=$(echo $url | awk -F/ '{print $3}')
+ip_address=$(dig +short $domain)
+echo "The IP of $domain is $ip_address"
+geolocation=$(curl -s ipinfo.io/$ip_address)
+echo "The geolocation of $ip_address is $geolocation"
+
+curl -v $url
