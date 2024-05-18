610c1bb4ea042d7fefed47a85d2aa1ea0351bb05
Jizu Sun <jizu.sun@philips.com>
2024-05-18 07:45:22 +0000

    dns resolve with opendns resolver

--- .github/scripts/get-server-geolocation.sh
+++ .github/scripts/get-server-geolocation.sh
@@ -5,10 +5,8 @@ if [ -z "$1" ]; then
 fi
 url=$1
 echo "The URL is $url"
-domain=$(echo $url | awk -F/ '{print $3}')
-ip_address=$(dig +short $domain)
+domain=$(echo "$url" | awk -F/ '{print $3}')
+ip_address=$(dig +short "$domain" @resolver1.opendns.com)
 echo "The IP of $domain is $ip_address"
-geolocation=$(curl -s ipinfo.io/$ip_address)
+geolocation=$(curl -s "ipinfo.io/$ip_address")
 echo "The geolocation of $ip_address is $geolocation"
-
-curl -v $url
